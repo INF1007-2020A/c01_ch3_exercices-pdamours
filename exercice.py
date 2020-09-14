107c9670c7416b8495492b33c8ef2e768622ca78
pdamours <philippe-1.damours@polymtl.ca>
2020-09-14 18:53:10 +0000

    PD: Completed the exercices

--- exercice.py
+++ exercice.py
@@ -6,23 +6,27 @@ import math
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a + b + c) / 3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
-
+    return math.radians(angle_degs + angle_mins / 60 + angle_secs / 3600)_
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    deg = int(math.degrees(angle_rads))
+
+    minutes = (math.degrees(angle_rads) - deg) * 60
+    seconds = (math.degrees(angle_rads) - (deg + minutes / 60)) * 60
+
+    return deg, minutes, seconds
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return temperature*(5/9) - 32
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return temperature*(9/5) + 32
 
 
 def main() -> None:
